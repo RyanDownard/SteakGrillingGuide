fe23eb06287bb64571c59f7ff492f2f9401c40ce
Ryan Downard <ryan.downard@aapc.com>
2023-06-12 14:43:16 -0600

    Attempt to clean up project

--- StreakGrillingGuide/StreakGrillingGuide/MainPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StreakGrillingGuide;
-
-public partial class MainPage : ContentPage
-{
-	public MainPage()
-	{
-		InitializeComponent();
-	}
-}
--- StreakGrillingGuide/StreakGrillingGuide/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace StreakGrillingGuide;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
